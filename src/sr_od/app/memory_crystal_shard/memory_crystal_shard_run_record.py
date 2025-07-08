8c89f9630579950cbbc5b2b6838acf2585789a96
MLGShimadaYasuo <134863853+MLGShimadaYasuo@users.noreply.github.com>
2025-07-09 07:01:48 +0800

    Update memory_crystal_shard_run_record.py

--- src/sr_od/app/memory_crystal_shard/memory_crystal_shard_run_record.py
+++ src/sr_od/app/memory_crystal_shard/memory_crystal_shard_run_record.py
@@ -7,4 +7,5 @@ class MemoryCrystalShardRunRecord(AppRunRecord):
 
     def __init__(self, instance_idx: Optional[int] = None, game_refresh_hour_offset: int = 0):
         AppRunRecord.__init__(self, 'memory_crystal_shard', instance_idx=instance_idx,
+                              record_period=AppRunRecordPeriod.WEEKLY,
                               game_refresh_hour_offset=game_refresh_hour_offset)
