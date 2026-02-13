43b1799610dd8612cb267550f253811b1539cef7
Usagi-wusaqi <2181802336@qq.com>
2026-02-13 20:02:31 +0800

    perf: 委托识别到派遣中立即返回成功，优化代码

--- src/sr_od/app/assignments/assignments_app.py
+++ src/sr_od/app/assignments/assignments_app.py
@@ -1,24 +1,22 @@
-from cv2.typing import MatLike
 from typing import ClassVar
 
-from one_dragon.base.matcher.match_result import MatchResult, MatchResultList
 from one_dragon.base.operation.operation_edge import node_from
 from one_dragon.base.operation.operation_node import operation_node
 from one_dragon.base.operation.operation_round_result import OperationRoundResult
-from one_dragon.utils import cv2_utils
 from one_dragon.utils.i18_utils import gt
 from sr_od.app.sr_application import SrApplication
 from sr_od.context.sr_context import SrContext
 from sr_od.operations.back_to_normal_world_plus import BackToNormalWorldPlus
-from sr_od.operations.menu import phone_menu_utils, phone_menu_const
+from sr_od.operations.menu import phone_menu_const, phone_menu_utils
 from sr_od.operations.menu.open_phone_menu import OpenPhoneMenu
 
 
 class AssignmentsApp(SrApplication):
+
     STATUS_WITH_ALERT: ClassVar[str] = '委托红点'
     STATUS_NO_ALERT: ClassVar[str] = '无委托红点'
-    STATUS_NO_ALL_CLAIM: ClassVar[str] = '无一键领取'
-    STATUS_NO_CLAIM: ClassVar[str] = '无领取'
+    STATUS_HAS_REWARD: ClassVar[str] = '领取奖励'
+    STATUS_ASSIGNING: ClassVar[str] = '委托派遣中'
 
     def __init__(self, ctx: SrContext):
         SrApplication.__init__(self, ctx, 'assignments', op_name=gt('委托'),
@@ -40,25 +38,49 @@ class AssignmentsApp(SrApplication):
     @operation_node(name='点击委托')
     def _click_assignment(self) -> OperationRoundResult:
         screen = self.last_screenshot
-        result: MatchResult = phone_menu_utils.get_phone_menu_item_pos(self.ctx, screen, phone_menu_const.ASSIGNMENTS, alert=False)
+        result = phone_menu_utils.get_phone_menu_item_pos(
+            self.ctx, screen, phone_menu_const.ASSIGNMENTS, alert=False
+        )
         if result is None:
-            return self.round_retry()
-        else:
-            self.ctx.controller.click(result.center)
-            return self.round_success(AssignmentsApp.STATUS_WITH_ALERT, wait=2)
+            return self.round_success(AssignmentsApp.STATUS_NO_ALERT)
 
-    @node_from(from_name='点击委托')
-    @operation_node(name='一键领取')
-    def _claim_all(self) -> OperationRoundResult:
+        self.ctx.controller.click(result.center)
+        return self.round_success(AssignmentsApp.STATUS_WITH_ALERT, wait=2)
+
+    @node_from(from_name='点击委托', status=STATUS_WITH_ALERT)
+    @node_from(from_name='点击空白处关闭')
+    @operation_node(name='识别委托状态')
+    def _check_status(self) -> OperationRoundResult:
         screen = self.last_screenshot
-        result = self.round_by_find_area(screen, '菜单', '委托-一键领取')
-        if result.is_success:
-            return self.round_by_find_and_click_area(screen, '菜单', '委托-一键领取',
-                                                     success_wait=1, retry_wait=1)
-        else:
-            return self.round_success(status=AssignmentsApp.STATUS_NO_ALL_CLAIM)
 
-    @node_from(from_name='一键领取')
+        result = self.round_by_find_area(screen, '菜单', '委托-领取奖励')
+        if result.is_success:
+            return self.round_success(AssignmentsApp.STATUS_HAS_REWARD)
+
+        result = self.round_by_find_area(screen, '菜单', '委托-委托派遣中')
+        if result.is_success:
+            return self.round_success(AssignmentsApp.STATUS_ASSIGNING)
+
+        return self.round_retry(wait=1)
+
+    @node_from(from_name='识别委托状态', status=STATUS_HAS_REWARD)
+    @operation_node(name='领取奖励')
+    def _claim_reward(self) -> OperationRoundResult:
+        screen = self.last_screenshot
+        return self.round_by_find_and_click_area(
+            screen, '菜单', '委托-领取奖励', success_wait=1, retry_wait=1
+        )
+
+    @node_from(from_name='领取奖励')
+    @operation_node(name='点击空白处关闭')
+    def _click_empty(self) -> OperationRoundResult:
+        screen = self.last_screenshot
+        return self.round_by_find_and_click_area(
+            screen, '菜单', '委托-点击空白处关闭', success_wait=1, retry_wait=1
+        )
+
+    @node_from(from_name='识别委托状态', status=STATUS_ASSIGNING)
+    @node_from(from_name='点击委托', status=STATUS_NO_ALERT)
     @operation_node(name='完成后返回大世界')
     def back_at_last(self) -> OperationRoundResult:
         self.notify_screenshot = self.save_screenshot_bytes()  # 结束后通知的截图
